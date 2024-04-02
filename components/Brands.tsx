fde20d9c2ec6ab6864de50c92e1b93ec4d54d656
ArfinHasib <arfinhasib21375@gmail.com>
2024-04-02 23:46:08 +0600

    All the components and section initialize

---
+++ components/Brands.tsx
@@ -0,0 +1,5 @@
+const Brands = () => {
+   return <section className='h-[25vh] bg-orange-300'>Brands</section>;
+};
+
+export default Brands;
---
+++ components/Testimonial.tsx
@@ -0,0 +1,5 @@
+const Testimonial = () => {
+   return <section className='h-[50vh] bg-pink-300'>Testimonial</section>;
+};
+
+export default Testimonial;
